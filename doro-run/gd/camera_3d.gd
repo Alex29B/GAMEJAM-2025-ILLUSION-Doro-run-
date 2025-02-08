022514b06580c38f38e78bac64977a3974549b67
alexandre-gia-linh tran <alexandre-gia-linh.tran@epitech.eu>
2025-02-08 11:30:00 +0100

    [ADD] player movement script and assets lib

---
+++ doro-run/gd/camera_3d.gd
@@ -0,0 +1,15 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate(global_transform.basis.y.normalized(), event.relative.x * 0.005)
---
+++ doro-run/gd/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const SENSITIVITY = 0.005
+const SPRINT_SPEED = 10.0
+
+@onready var head = $Head2
+@onready var camera = $Head2/Camera3D
+
+var speed = SPEED
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(50))
+
+func _physics_process(delta: float) -> void:
+	#Handle sprint
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = SPEED
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
