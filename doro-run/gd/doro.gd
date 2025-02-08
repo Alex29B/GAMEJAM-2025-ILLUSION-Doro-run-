0beb37f74ccfd93b6c90bc10e9623c9ab8a3d6d1
alexandre-gia-linh tran <alexandre-gia-linh.tran@epitech.eu>
2025-02-08 15:39:36 +0100

    [ADD] Enemy pathfinding

---
+++ doro-run/gd/doro.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+const SPEED = .1
+const JUMP = .5
+
+@onready var nav_agent = $NavigationAgent3D
+
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	var current_location = global_transform.origin
+	if (nav_agent.distance_to_target() > 1):
+		var prev_location = current_location
+		if current_location.x > nav_agent.target_position.x:
+			current_location.x -= SPEED
+		else:
+			current_location.x += SPEED
+		if current_location.z > nav_agent.target_position.z:
+			current_location.z -= SPEED
+		else:
+			current_location.z += SPEED
+		if ((prev_location.x - current_location.x) + (prev_location.z - current_location.z)) < 1:
+			current_location.y += JUMP
+	rotate_y(0.1)
+	global_position = global_position.move_toward(current_location, SPEED)
+	move_and_slide()
