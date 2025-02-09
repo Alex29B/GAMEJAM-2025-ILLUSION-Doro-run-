3cd7670947464e0b07ddf4ad9e60fc91da58a8bf
alexandre-gia-linh tran <alexandre-gia-linh.tran@epitech.eu>
2025-02-09 02:56:03 +0100

    [DEL] duplicate files

--- doro-run/gd/world.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@onready var player = $Player
-
-# Called when the node enters the scene tree for the first time.
-func _physics_process(delta):
-	get_tree().call_group('enemies', 'update_target_location', player.global_transform.origin)
